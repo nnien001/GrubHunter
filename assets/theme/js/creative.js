d861585e28354fb1c8c203c09f0154b4786ac387
de0lan10 <deonte.lang@gmail.com>
2016-11-14 20:59:24 -0800

    added extra js links. Page broken. Must fix magnific undefined

--- assets/theme/js/creative.js
+++ assets/theme/js/creative.js
@@ -62,3 +62,18 @@
     });
 
 })(jQuery); // End of use strict
+
+$('.carousel-inner .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
