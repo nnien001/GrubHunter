9b0280c653fe7d8945c78d2d2e795165618b685a
nnien001 <nnien001@gmail.com>
2016-11-15 20:48:01 -0800

    map clearing works now. Can repeatedly put in new zips for new markers

--- assets/javascript/googlemaps.js
+++ assets/javascript/googlemaps.js
@@ -1,7 +1,7 @@
 console.log("using googlemaps.js")
 
 var map = null; //to be initialized by initMap. this is a global.
-//var marker = null;
+var markerArray = []; //holds your markers
 
 var la = { //our map center.
 	name: "los angeles",
@@ -11,7 +11,7 @@ var la = { //our map center.
 
 function initMap(newZip) { //initialize map the first time or center map if already initialized
   
-  if (map == null) {
+  if (map == null) { //map not yet initialized, build new map.
 	 geocoder = new google.maps.Geocoder();
  	 map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 10,
@@ -19,23 +19,20 @@ function initMap(newZip) { //initialize map the first time or center map if alre
 	 });
 
   }
+  else { //putting in new zip after map initialized
+    console.log("clear marker here");
+    markerClear();
+  }
 
- 	geocoder.geocode( { 'address': newZip}, function(results, status) {
+ 	geocoder.geocode( { 'address': newZip}, function(results, status) { //get GPS of zip, zoom on zip.
   		if (status == 'OK') {
-
     		map.setCenter(results[0].geometry.location);
-    		var marker = new google.maps.Marker({
-        	map: map,
-        	position: results[0].geometry.location
-    	});
+
   		} else {
     		alert('Geocode was not successful for the following reason: ' + status);
   		}
 	});
 
-  map.addListener('click', function(event) {
-    console.log("map hit");
-  });
 
 
 }
@@ -45,10 +42,11 @@ function addNewMarker(newPos, newLabel) {
 			position: newPos,
 			map: map,
 			label: newLabel
-		});
+	});
+
+  markerArray.push(marker);
 
   marker.addListener('click', function(event) {
-    console.log("marker hit", newPos);
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -60,20 +58,31 @@ function addNewMarker(newPos, newLabel) {
         }
       };
       
-      var url = "https://www.google.com/maps/dir/" 
-                  + newPos.lat + "," + newPos.lng + "/" +geoPos.pos.lat + "," + geoPos.pos.lng; 
+      var url = "https://www.google.com/maps/dir/"+ newPos.lat + "," + newPos.lng + "/" +geoPos.pos.lat + "," + geoPos.pos.lng; 
 
-      //console.log(url);
       location.href=url;
 
       }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
     } 
+    
     else {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
-    } 
+    }
+
   });
-  
+
+}
+
+function setMapOnAll(map) { //typically not used other than to set to null
+  for (var i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(map);
+  }
+}
+
+function markerClear() {
+  setMapOnAll(null); //go through all marker positions, set to null
+  markerArray = []; //wipe all marker references from the array
 }
